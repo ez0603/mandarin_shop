470aa9596885d8bcd657908d13b0f29476e669d4
ez0603 <dlwldjs3132@naver.com>
2024-08-03 17:29:22 +0900

    Feat : 인스턴스

---
+++ src/apis/utils/instance.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import getServerAddress from "../../constants/serverAddress";
+
+export const instance = axios.create({
+    baseURL: getServerAddress(),
+    headers: {
+        Authorization: "Bearer " + localStorage.getItem("AccessToken"),
+    },
+});
---
+++ src/constants/serverAddress.js
@@ -0,0 +1,6 @@
+const LOCAL_ADDRESS = "http://localhost:8080";
+const DEPLOY_ADDRESS = "";
+
+export default function getServerAddress() {
+    return LOCAL_ADDRESS;
+}
